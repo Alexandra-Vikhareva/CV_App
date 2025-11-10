f81aa72f135baf839d6cad7cf86b1b9b1e9369f0
Sasha <st087955@student.spbu.ru>
2025-11-11 01:09:30 +0300

    Добавлены плейсхолдеры для образования

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,19 +6,22 @@ export function Education({onInputChange}) {
                 <label htmlFor="university">Учебное заведение: </label>
                 <input type="text" 
                 name="university" 
-                id="university" 
+                id="university"
+                placeholder="МГУ" 
                 onChange={(e) => onInputChange('university', e.target.value)}/>
 
                 <label htmlFor="specialization">Направление: </label>
                 <input type="text" 
                 name="specialization" 
                 id="specialization" 
+                placeholder="ПМИ" 
                 onChange={(e) => onInputChange('specialization', e.target.value)}/>
 
                 <label htmlFor="graduationYear">Год окончания: </label>
                 <input type="number" 
                 name="graduationYear" 
                 id="graduationYear" 
+                placeholder="2025" 
                 onChange={(e) => onInputChange('graduationYear', e.target.value)}/>
             </form>
         </div>
