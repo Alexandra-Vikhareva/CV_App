dca9599ed983df1779e70fb0a33bed9217300651
Sasha <st087955@student.spbu.ru>
2025-11-11 00:54:28 +0300

    Добавлены стили

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
 import './App.css'
+
+import { WorkExperience } from './components/WorkExperience'
+import { Education } from './components/Education'
 import { GeneralInformation } from './components/GeneralInformation'
+import { Resume } from './components/Resume'
+import { useState } from 'react'
+import { Skill } from './components/Skill'
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: '',
+    secondName: '',
+    position: '',
+    phone: '',
+    email: ''
+  });
+
+  const [educationInfo, setEducationInfo] = useState({
+    university: '',
+    specialization: '',
+    graduationYear: '',
+  });
+
+  const [workExperienceInfo, setWorkExperianceInfo] = useState({
+    company: '',
+    position: '',
+    startDate: '',
+    endDate: ''
+  })
+
+  const [skills, setSkills] = useState('')
+
+  function handleGeneralInfoChange(field, value) {
+    setGeneralInfo(prev => ({ ...prev, [field]:value}))
+  }
+
+  function handleEducationInfoChange(field, value) {
+    setEducationInfo(prev => ({ ...prev, [field]:value}))
+  }
+
+  function handleWorkExperienceInfoChange(field, value) {
+    setWorkExperianceInfo(prev => ({ ...prev, [field]:value}))
+  }
+
+  function handleChange(value) {
+    setSkills(value)
+  }
 
   return (
-    <>
-    <GeneralInformation/>
-    </>
+    <div className='content'>
+      <div className="forms">
+        <GeneralInformation onInputChange={handleGeneralInfoChange}/>
+        <Education onInputChange={handleEducationInfoChange}/>
+        <WorkExperience onInputChange={handleWorkExperienceInfoChange}/>
+        <Skill onInputChange={handleChange}/>
+      </div>
+      <div className="sheet">
+        <Resume props={{generalInfo, educationInfo, workExperienceInfo, skills}}/>
+      </div>
+    </div>
   )
 }
 
